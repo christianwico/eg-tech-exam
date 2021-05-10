e886124d27a638b701fcfe9ba7444eb89caf5a51
Christian Neil Anthony Wico <cnawico@gmail.com>
2021-05-10 20:05:29 +0800

    Create Landing page layout

--- lib/pages/landing/landing_page.dart
+++ lib/pages/landing/landing_page.dart
@@ -17,11 +17,62 @@ class LandingPage extends StatelessWidget {
           vertical: 24.0,
         ),
         width: double.maxFinite,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [],
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(top: 24.0),
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 48.0,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: 48.0,
+                    ),
+                    radius: 44.0,
+                  ),
+                ],
+              ),
+              Card(
+                child: ListView(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: [
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                    _buildListTile('Testing for layout', 'Name'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildListTile(String title, String subtitle) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.black),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: TextStyle(color: Colors.grey),
       ),
     );
   }
