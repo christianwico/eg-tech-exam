4c6abf70511e89ee59b7dfe2e42c798b3d0e8088
Christian Neil Anthony Wico <cnawico@gmail.com>
2021-05-10 17:04:12 +0800

    Create Auth0 constants

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+const AUTH0_DOMAIN = 'cwic-personal.us.auth0.com';
+const AUTH0_CLIENT_ID = 'UspTEZVqbR6LmOFVNMkphj8OT5bnR3hQ';
+const AUTH0_REDIRECT_URI = 'au.com.eg.exam://login-callback';
+const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
